613c1b98b3588fdded9f59c06220d024971d67ff
mayanksaini18 <mayanksaini0416@gmail.com>
2026-04-19 18:48:43 +0530

    refactor: simplify root layout for route group support

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 import "@/app/globals.css";
-import Header from "@/components/layout/Header";
-import Sidebar from "@/components/layout/Sidebar";
 import { ThemeProvider } from "@/components/layout/ThemeProvider";
 
 export const metadata: Metadata = {
@@ -22,13 +20,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className="antialiased">
           <ThemeProvider>
-            <div className="min-h-screen lg:flex">
-              <Sidebar />
-              <div className="flex min-h-screen min-w-0 flex-1 flex-col">
-                <Header />
-                <main className="flex-1 px-4 py-6 sm:px-6">{children}</main>
-              </div>
-            </div>
+            {children}
             <Toaster richColors position="top-right" />
           </ThemeProvider>
         </body>
